28fe007986734b4817d0e44ac00a81a9b425e74b
Blanka Bartoňová <bbartonova@gmail.com>
2021-05-22 11:47:08 +0200

    15. úkol řešení final

--- src/index.jsx
+++ src/index.jsx
@@ -1,62 +1,23 @@
-import React, { useState } from 'react';
+import React from 'react';
 import { render } from 'react-dom';
 import './style.css';
 
-import { Joke } from '../src/joke/Joke';
+import { Joke } from './joke/Joke';
+import { jokes } from '../jokes';
 
 const App = () => {
-  const [likeCount, setLikeCount] = useState(0);
-  const [dislikeCount, setDislikeCount] = useState(0);
-
   return (
     <div className="container">
-      <header></header>
+      <header>
+        <h2>All jokes</h2>
+      </header>
       <main>
         <div className="container">
-          <div className="joke">
-            <div className="joke__body">
-              <div className="joke__user">
-                <img
-                  className="user-avatar"
-                  src="https://raw.githubusercontent.com/Czechitas-podklady-WEB/dadjokes/main/users/user01.png"
-                />
-                <p className="user-name">Neroxx</p>
-              </div>
-
-              <p className="joke__text">
-                The secret service isn't allowed to yell "Get down!" anymore
-                when the president is about to be attacked. Now they have to
-                yell "Donald, duck!"
-              </p>
-            </div>
-            <div className="joke__likes">
-              <button
-                id="btn-up"
-                className="btn-like btn-like--up"
-                onClick={() => {
-                  setLikeCount(likeCount + 1);
-                }}
-              ></button>
-              <span id="likes-up" className="likes-count likes-count--up">
-                {likeCount}
-              </span>
-              <button
-                id="btn-down"
-                className="btn-like btn-like--down"
-                onClick={() => {
-                  setDislikeCount(dislikeCount + 1);
-                }}
-              ></button>
-              <span id="likes-down" className="likes-count likes-count--down">
-                {dislikeCount}
-              </span>
-            </div>
-          </div>
+          {jokes.map((joke, index) => (
+            <Joke key={index} props={joke} />
+          ))}
         </div>
-
-        <Joke />
       </main>
-      <footer></footer>
     </div>
   );
 };
--- src/joke/Joke.jsx
+++ src/joke/Joke.jsx
@@ -1,26 +1,48 @@
-import React, { useState } from 'react';
+import React, {useState} from 'react';
 import '../joke/style.css';
-import { jokes } from 'jokes.js';
 
-export const Joke = (props) => {
-  return (
-    <>
-      <h2>Přehled vtipů</h2>
-      <ul>
-        {props.jokes.map((joke) => (
-          <Joke
-            key={jokes.id}
-            id={jokes.id}
-            userAvatar={jokes.avatar}
-            userName={jokes.name}
-            text={jokes.text}
-            likes={jokes.likes}
-            dislikes={jokes.dislikes}
-          ></Joke>
-        ))}
-      </ul>
-    </>
-  );
+export const Joke = ( {props} ) => {
+    const [likeCount, setLikeCount] = useState(props.likes);
+    const [dislikeCount, setDislikeCount] = useState(props.dislikes);
+
+    return (
+        <>
+            <div className="joke">
+                <div className="joke__body">
+                    <div className="joke__user">
+                        <img
+                            className="user-avatar"
+                            src={props.avatar}
+                        />
+                        <p className="user-name">{props.name}</p>
+                    </div>
+                    <p className="joke__text">
+                        {props.text}
+                    </p>
+                </div>
+                <div className="joke__likes">
+                    <button
+                        id="btn-up"
+                        className="btn-like btn-like--up"
+                        onClick={() => {
+                            setLikeCount(likeCount + 1);
+                        }}
+                    ></button>
+                    <span id="likes-up" className="likes-count likes-count--up">
+                {likeCount}
+              </span>
+                    <button
+                        id="btn-down"
+                        className="btn-like btn-like--down"
+                        onClick={() => {
+                            setDislikeCount(dislikeCount + 1);
+                        }}
+                    ></button>
+                    <span id="likes-down" className="likes-count likes-count--down">
+                {dislikeCount}
+              </span>
+                </div>
+            </div>
+        </>
+    );
 };
-
-render(<Joke />, document.querySelector('#app'));
